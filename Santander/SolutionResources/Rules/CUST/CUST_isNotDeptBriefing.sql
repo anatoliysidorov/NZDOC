e96987a6537abda3f1be215e5c6b2f2795ce05d5
anatoliysidorov <anatoliy.sidorov@gmail.com>
2019-01-17 11:36:32 +0200

    https://jira.appbase.com/browse/SAL-639

---
+++ Santander/SolutionResources/Rules/CUST/CUST_isNotDeptBriefing.sql
@@ -0,0 +1,41 @@
+DECLARE
+	--input
+	v_TaskDependencyId INTEGER;
+	
+	--system
+	v_result INTEGER;
+
+	v_parentTask INTEGER;
+	v_parentTaskState INTEGER;
+	v_parentTaskCase INTEGER;
+	v_parentTaskCustomData NCLOB;
+
+	v_childTask INTEGER;
+	v_childTaskState INTEGER;
+	v_childTaskCase INTEGER;
+	v_childTaskCustomData NCLOB;
+BEGIN
+	v_TaskDependencyId := :TaskDependencyId;
+	
+	--get info about parent task (the one that has the math test)
+	v_result := f_DCM_getDependencyInfo(
+		TaskDependencyId => v_TaskDependencyId,
+		childTask => v_childTask,
+		childTaskCase => v_childTaskCase,
+		childTaskCustomData => v_childTaskCustomData,
+		childTaskState => v_childTaskState, 
+		parentTask => v_parentTask,
+		parentTaskCase => v_parentTaskCase,
+		parentTaskCustomData => v_parentTaskCustomData,
+		parentTaskState => v_parentTaskState
+	);
+    
+    select count(1) into :TaskResult
+    from tbl_Task t 
+    join tbl_cdm_briefings cb on cb.COL_BRIEFINGSCASE = t.COL_CASETASK
+    join tbl_dict_customword cw on cw.col_id = cb.COL_CDM_BRIEFINGBRIEFING_PRO AND UPPER(cw.col_Code) != 'DEPARTMENTAL_BRIEFING'
+    join tbl_dict_customcategory cc on cw.col_wordcategory = cc.col_id AND UPPER(cc.col_code) = 'BRIEFING_PRODUCT'
+    where t.col_id = v_parentTask;
+    
+	RETURN :TaskResult;
+END;
