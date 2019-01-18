4fcec8b52a01f159d7eaa28a9479ba1e0dbb7348
anatoliysidorov <anatoliy.sidorov@gmail.com>
2019-01-18 11:15:54 +0200

    https://jira.appbase.com/browse/SAL-640

---
+++ Santander/SolutionResources/Rules/CUST/CUST_ltr_gen_by_code.cs
@@ -0,0 +1,22 @@
+Func<string, bool, string> getParam = (n, r) => Ecx.Rule.Helpers.BDSHelper.GetParameterAsString(request, n, r);
+var token = getParam("TOKEN", true);
+var domain = getParam("DOMAIN", true);
+var taskId = getParam("TaskId", false);
+var name = getParam("Name", false);
+var ruleCode = "root_ltr_gen_by_code";
+var inputParams = request.Parameters.Where(p => p.Name != "t").ToDictionary(p => p.Name, p => p.Value == null ? null : p.Value.ToString());
+
+Func<XDocument, string, string> getValueFromResponse = LTR.Helpers.AppBaseHelper.GetValueFromResponse;
+Func<string, Dictionary<string, string>, XDocument> executeRule = (c, p) => LTR.Helpers.AppBaseHelper.ExecuteRuleXml(token, domain, c, p);
+
+//get Case Id
+var caseIdRes = executeRule("root_ltr_dcm_task_get_case_id", new Dictionary<string, string> { { "TaskId", taskId } });
+var caseId = getValueFromResponse(caseIdRes, "CASEID");
+inputParams["CASE_ID"] = caseId;
+inputParams["ObjectId"] = caseId;
+inputParams["Name"] = "19-B-"+ caseId + "-"+name;
+inputParams["ObjectType"] = "root_Case";
+var ltrGenRes = executeRule(ruleCode, inputParams);
+
+response.Result.AddParameter(new Parameter() { Name = "errorCode", Value = getValueFromResponse(ltrGenRes, "ERRORCODE") });
+response.Result.AddParameter(new Parameter() { Name = "errorMessage", Value = getValueFromResponse(ltrGenRes, "ERRORMESSAGE") });
