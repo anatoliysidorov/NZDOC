771f1181caf48966bb241f6f1bd28c58bb594be0
anatoliysidorov <anatoliy.sidorov@gmail.com>
2019-01-18 11:17:45 +0200

    Rule to get Product Type by Task Id. https://jira.appbase.com/browse/SAL-640

---
+++ Santander/SolutionResources/Rules/CUST/CUST_getProductType.sql
@@ -0,0 +1,5 @@
+select cw.col_Name
+    from tbl_Case c 
+    join tbl_cdm_briefings cb on cb.COL_BRIEFINGSCASE = c.COL_ID
+    join tbl_dict_customword cw on cw.col_id = cb.COL_CDM_BRIEFINGBRIEFING_PRO
+    where c.col_id = (select col_CaseTask as CASEID from tbl_Task where col_id = :TaskId)
